0720f35a8ee2bcef07b25e312af912b2358d758b
afrin <afrinhaque@outlook.com>
2023-04-26 14:57:59 +0600

    Deployement changes added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from "react";
 import Nav from "./components/Nav";
 import Market from "./components/Market";
 import ProductPage from "./components/ProductPage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Router basename={process.env.PUBLIC_URL}>
-        <Switch>
+        <Routes>
           <Route path="/" element={<Market />} />
           <Route path="/products/:id" element={<ProductPage />} />
-        </Switch>
+        </Routes>
       </Router>
     </div>
   );
